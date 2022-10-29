0f7f1e15049af0e43b5952aa4e7da7d0d4409eb5
Merugu Venkatesh <meruguvenkatesh96@gmail.com>
2022-10-29 15:48:50 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from "react";
 
 function App() {
+
+  const [data,setData]=useState({
+    email:'',
+    password:''
+  })
+  const{email,password}=data;
+  const changeHandler =e =>{
+    setData({...data,[e.target.name]:[e.target.value]})
+  }
+
+const submitHandler = e=>{
+  console.log("Successfully Added")
+}
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <div className="log">
+      <form onSubmit={submitHandler}>
+        <div className="ma">
+        <label>Email :</label> <br></br><br></br>
+        <input type="text" name="email" placeholder="Enter Email" value={email} onChange={changeHandler}/><br></br><br></br>
+        <span>Password :</span> <br></br><br></br>
+        <input type="password" name="password" placeholder="Enter Password" value={password} onChange={changeHandler}/><br></br><br></br>
+        <input type="submit" name="submit"/>
+        </div>
+      </form>
+      </div>
     </div>
   );
 }
